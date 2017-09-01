d0cd4e5a21787d2a7c77e423e378fb5e59e5a588
brian <brian@logos>
2017-08-31 20:16:00 -0400

    Bare minimum

--- src/interfaces/main.cpp
+++ src/interfaces/main.cpp
@@ -1,67 +1,79 @@
+#include <cerrno> // cerrno
+#include <cstdint>
+#include <cstdio> // std::perror, std::fprintf, std::printf
+#include <cstdlib>
+#include <cstring> // std::strerror
+#include <string> // std::string
 #include <arpa/inet.h>
-#include <sys/socket.h>
-#include <netdb.h>
-#include <ifaddrs.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include <ifaddrs.h> // ::freeifaddrs, ::getifaddrs
+#include <linux/if_link.h> // rtnl_link_stats
+#include <netdb.h> // ::getnameinfo, NI_MAXHOST, NI_MAXSERV
+#include <sys/socket.h> // ::getnameinfo
+#include <sys/types.h> // ::freeifaddrs, ::getifaddrs
 #include <unistd.h>
-#include <linux/if_link.h>
 
-int main(int, char**)
-{
-    struct ifaddrs *ifaddr, *ifa;
-    int family, s, n;
-    char host[NI_MAXHOST];
+const char*
+family_to_string(int family) {
+    switch(family) {
+        case AF_PACKET: return "AF_PACKET";
+        case AF_INET: return "AF_INET";
+        case AF_INET6: return "AF_INET6";
+        default: return "?";
+    }
+}
 
-    if (getifaddrs(&ifaddr) == -1) {
-        perror("getifaddrs");
-        exit(EXIT_FAILURE);
+struct Interface {
+    std::string name;
+    int flags;
+    int family;
+    std::string address;
+    std::string service;
+    uint32_t tx_packets;
+    uint32_t rx_packets;
+    uint32_t tx_bytes;
+    uint32_t rx_bytes;
+};
+
+
+int main(int, char**) {
+    ifaddrs* ifaddr;
+
+    int rv = ::getifaddrs(&ifaddr);
+    if (rv == -1) {
+        std::fprintf(stderr, "Error: getifaddrs: %s", std::strerror(errno));
+        return 1;
     }
 
-    /* Walk through linked list, maintaining head pointer so we
-        can free list later */
+    for (ifaddrs* ifa = ifaddr; ifa != nullptr; ifa = ifa->ifa_next) {
+        if (ifa->ifa_addr == nullptr) continue;
 
-    for (ifa = ifaddr, n = 0; ifa != NULL; ifa = ifa->ifa_next, n++) {
-        if (ifa->ifa_addr == NULL)
-            continue;
-
-        family = ifa->ifa_addr->sa_family;
-
-        /* Display interface name and family (including symbolic
-            form of the latter for the common families) */
-
-        printf("%-8s %s (%d)\n",
-                ifa->ifa_name,
-                (family == AF_PACKET) ? "AF_PACKET" :
-                (family == AF_INET) ? "AF_INET" :
-                (family == AF_INET6) ? "AF_INET6" : "???",
-                family);
-
-        /* For an AF_INET* interface address, display the address */
+        int family = ifa->ifa_addr->sa_family;
+        char host[NI_MAXHOST] = {};
+        char service[NI_MAXSERV] = {};
+        unsigned tx_packets = 0, rx_packets = 0;
+        unsigned tx_bytes = 0, rx_bytes = 0;
 
         if (family == AF_INET || family == AF_INET6) {
-            s = getnameinfo(ifa->ifa_addr,
-                    (family == AF_INET) ? sizeof(struct sockaddr_in) :
-                                            sizeof(struct sockaddr_in6),
-                    host, NI_MAXHOST,
-                    nullptr, 0, NI_NUMERICHOST);
-            if (s != 0) {
-                printf("getnameinfo() failed: %s\n", gai_strerror(s));
-                exit(EXIT_FAILURE);
+            rv = getnameinfo(ifa->ifa_addr, sizeof(sockaddr_storage), host, NI_MAXHOST,
+                    service, NI_MAXSERV, NI_NUMERICHOST);
+            if (rv != 0) {
+                std::fprintf(stderr, "Error: getnameinfo: %s\n", ::gai_strerror(rv));
+                return 1;
             }
-
-            printf("\t\taddress: <%s>\n", host);
-
         } else if (family == AF_PACKET && ifa->ifa_data != NULL) {
             rtnl_link_stats* stats = static_cast<rtnl_link_stats*>(ifa->ifa_data);
-
-            printf("\t\ttx_packets = %10u; rx_packets = %10u\n"
-                    "\t\ttx_bytes   = %10u; rx_bytes   = %10u\n",
-                    stats->tx_packets, stats->rx_packets,
-                    stats->tx_bytes, stats->rx_bytes);
+            tx_packets = stats->tx_packets;
+            rx_packets = stats->rx_packets;
+            tx_bytes = stats->tx_bytes;
+            rx_bytes = stats->rx_bytes;
         }
-    }
 
-    freeifaddrs(ifaddr);
-    exit(EXIT_SUCCESS);
+
+        std::printf("name=%s,flags=0x%x,family=%s,address=%s,service=%s,tx_packets=%u,rx_packets=%u,tx_bytes=%u,rx_bytes=%u\n",
+                    ifa->ifa_name, ifa->ifa_flags, ::family_to_string(family), host, service,
+                    tx_packets, rx_packets, tx_bytes, rx_bytes);
+    } // for
+
+    ::freeifaddrs(ifaddr);
+    std::exit(EXIT_SUCCESS);
 }
