ccffcc22fb1d48c73f75a0cb354831f61cc4aeab
brian <brian@eolas>
2020-10-11 10:41:54 -0400

    [interfaces] use libfmt

--- src/interfaces/main.cpp
+++ src/interfaces/main.cpp
@@ -1,35 +1,42 @@
 #include "util/net_util.hpp"
-#include <cinttypes> // PRI identifiers
-#include <cstdio>    // std::printf
+#include <fmt/format.h>
+#include <cstdio> // std::fprintf
 #include <vector>
 
-int
-main(int, char**)
-{
-    std::vector<net::interface> const interfaces = net::get_interfaces();
 
-    std::printf("Number of interfaces: %zu\n", interfaces.size());
-    for (auto const& i : interfaces) {
-        std::printf("%s\n    flags: 0x%x\n", i.name.c_str(), i.flags);
-        std::printf("    families: ");
-        for (auto const& f : i.families) {
-            std::printf("%s,", net::family_to_string(f));
+int
+main()
+{
+    try {
+        std::vector<net::interface> const interfaces = net::get_interfaces();
+
+        fmt::print(FMT_STRING("Number of interfaces: {}\n"), interfaces.size());
+        for (auto const& i : interfaces) {
+            fmt::print(FMT_STRING("{}\n    flags: {:#010x}\n"), i.name, i.flags);
+            fmt::print(FMT_STRING("    families: "));
+            for (auto const& f : i.families) {
+                fmt::print(FMT_STRING("{},"), net::family_to_string(f));
+            }
+            fmt::print(FMT_STRING("\n"));
+            for (auto const& a : i.addresses) {
+                std::string const& address = std::get<0>(a);
+                std::string const& service = std::get<1>(a);
+                if (!address.empty())
+                    fmt::print(FMT_STRING("    address: {}, service={}\n"), address, service);
+            }
+            fmt::print(FMT_STRING("    rx_packets={}, rx_bytes={}, rx_errors={}, rx_dropped={}\n"),
+                    i.stats.rx_packets, i.stats.rx_bytes, i.stats.rx_errors, i.stats.rx_dropped);
+            fmt::print(FMT_STRING("    tx_packets={}, tx_bytes={}, tx_errors={}, tx_dropped={}\n"),
+                    i.stats.tx_packets, i.stats.tx_bytes, i.stats.tx_errors, i.stats.tx_dropped);
+            fmt::print(FMT_STRING("    multicast={}\n"), i.stats.multicast);
         }
-        std::printf("\n");
-        for (auto const& a : i.addresses) {
-            std::string const& address = std::get<0>(a);
-            std::string const& service = std::get<1>(a);
-            if (!address.empty())
-                std::printf("    address: %s, service=%s\n", address.c_str(), service.c_str());
-        }
-        std::printf("    rx_packets=%" PRIu32 ", rx_bytes=%" PRIu32 ", rx_errors=%" PRIu32
-                    ", rx_dropped=%" PRIu32 "\n",
-                i.stats.rx_packets, i.stats.rx_bytes, i.stats.rx_errors, i.stats.rx_dropped);
-        std::printf("    tx_packets=%" PRIu32 ", tx_bytes=%" PRIu32 ", tx_errors=%" PRIu32
-                    ", tx_dropped=%" PRIu32 "\n",
-                i.stats.tx_packets, i.stats.tx_bytes, i.stats.tx_errors, i.stats.tx_dropped);
-        std::printf("    multicast=%" PRIu32 "\n", i.stats.multicast);
+    } catch (std::exception const& e) {
+        std::fprintf(stderr, "error: exception: %s\n", e.what());
+        return EXIT_FAILURE;
+    } catch (...) {
+        std::fprintf(stderr, "error: exception: ???\n");
+        return EXIT_FAILURE;
     }
 
-    return 0;
+    return EXIT_SUCCESS;
 }
