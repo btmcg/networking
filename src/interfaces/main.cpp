4893c2f46b3c52d684373c3fd11b5b75c6cea4f3
brian <brian@logos>
2025-07-18 16:04:39 -0400

    update interfaces to c++26 and rm fmt dep

--- src/interfaces/main.cpp
+++ src/interfaces/main.cpp
@@ -1,6 +1,6 @@
 #include "util/net_util.hpp"
-#include <fmt/format.h>
 #include <cstdio> // std::fprintf
+#include <print>
 #include <vector>
 
 
@@ -10,25 +10,26 @@ main()
     try {
         std::vector<net::interface> const interfaces = net::get_interfaces();
 
-        fmt::print(FMT_STRING("Number of interfaces: {}\n"), interfaces.size());
+        std::println("Number of interfaces: {}", interfaces.size());
         for (auto const& i : interfaces) {
-            fmt::print(FMT_STRING("{}\n    flags: {:#010x}\n"), i.name, i.flags);
-            fmt::print(FMT_STRING("    families: "));
+            std::println("{}\n    flags: {:#010x}", i.name, i.flags);
+            std::print("    families: ");
             for (auto const& f : i.families) {
-                fmt::print(FMT_STRING("{},"), net::family_to_string(f));
+                std::print("{},", net::family_to_string(f));
             }
-            fmt::print(FMT_STRING("\n"));
+            std::println();
             for (auto const& a : i.addresses) {
                 std::string const& address = std::get<0>(a);
                 std::string const& service = std::get<1>(a);
-                if (!address.empty())
-                    fmt::print(FMT_STRING("    address: {}, service={}\n"), address, service);
+                if (!address.empty()) {
+                    std::println("    address: {}, service={}", address, service);
+                }
             }
-            fmt::print(FMT_STRING("    rx_packets={}, rx_bytes={}, rx_errors={}, rx_dropped={}\n"),
+            std::println("    rx_packets={}, rx_bytes={}, rx_errors={}, rx_dropped={}",
                     i.stats.rx_packets, i.stats.rx_bytes, i.stats.rx_errors, i.stats.rx_dropped);
-            fmt::print(FMT_STRING("    tx_packets={}, tx_bytes={}, tx_errors={}, tx_dropped={}\n"),
+            std::println("    tx_packets={}, tx_bytes={}, tx_errors={}, tx_dropped={}",
                     i.stats.tx_packets, i.stats.tx_bytes, i.stats.tx_errors, i.stats.tx_dropped);
-            fmt::print(FMT_STRING("    multicast={}\n"), i.stats.multicast);
+            std::println("    multicast={}", i.stats.multicast);
         }
     } catch (std::exception const& e) {
         std::fprintf(stderr, "error: exception: %s\n", e.what());
